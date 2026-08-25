4662359910315e4024f0a9763e1d959b64dc78c6
Yash Tiwari <yash887120@gmail.com>
2026-08-25 16:13:26 +0530

    Fix CORS allow_credentials issue

--- services/assistant_api/main.py
+++ services/assistant_api/main.py
@@ -19,7 +19,7 @@ app.add_middleware(CorrelationIdMiddleware)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
